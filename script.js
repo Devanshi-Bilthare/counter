94e8ffb7f2e916d6be522c05745123b38bd9ef8e
Devanshi Bilthare <devanshibilthare@gmail.com>
2022-02-25 21:19:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let count = 0;
+
+ const span = document.querySelector('.span');
+ const btns = document.querySelectorAll(".btn");
+ 
+ btns.forEach( function(btn) {
+     btn.addEventListener("click", function(e){
+         const styles = e.currentTarget.classList;
+         if( styles.contains('decrease')){
+             count--;
+         }
+
+         else if( styles.contains('increase')){
+             count++;
+         }
+
+         else{
+             count = 0;
+         }
+
+         if(count > 0){
+             span.style.color = 'green';
+         }
+         if(count < 0){
+             span.style.color = 'red';
+         }
+        if(count == 0){
+            span.style.color = 'black';
+        }
+         span.textContent = count;
+         
+     });   
+ });
